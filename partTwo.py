e43d63ce32321ad102b55a4ef8564759686782d8
pg399 <pg399@kent.ac.uk>
2026-01-20 12:52:00 +0000

    Update partTwo.py

--- partTwo.py
+++ partTwo.py
@@ -1,8 +1,8 @@
-import math  
+import random 
 
 def main():
-#TO DO  
-
+    secret_number = random.randit(1, 10)  
+     guess = int(input(" Guess a number between 1 and 10: "))
 def pythag(A,B):
 #TO DO  
 
